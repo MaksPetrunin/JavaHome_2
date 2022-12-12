1d3bed58e409fd914a2e323b982a61e6d1e5331b
MaksPetrunin <57718767+MaksPetrunin@users.noreply.github.com>
2022-12-12 15:28:28 +0300

    Delete Ex5.java

--- Ex5.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.lab1;
-
-public class Ex5 {
-    public static void main(String[] args){
-        for(int i = 0; i < args.length; i++)
-            System.out.println(args[i]);
-    }
-}
